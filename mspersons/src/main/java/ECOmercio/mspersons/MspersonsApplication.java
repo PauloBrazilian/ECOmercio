dc565c390a4042d04f71588f0a6aa8136bb4d137
Paulo Ricardo <paulo.rdndc@gmail.com>
2024-07-24 15:08:28 -0300

    inicio do microservico msPersons

---
+++ mspersons/src/main/java/ECOmercio/mspersons/MspersonsApplication.java
@@ -0,0 +1,13 @@
+package ECOmercio.mspersons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspersonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspersonsApplication.class, args);
+	}
+
+}
---
+++ mspersons/src/test/java/ECOmercio/mspersons/MspersonsApplicationTests.java
@@ -0,0 +1,13 @@
+package ECOmercio.mspersons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspersonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
